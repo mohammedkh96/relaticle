cab1b1fb347af78e1b67ecfd9b16ba31028cbd94
Mohammed O. Ahmed <128855514+mohammedkh96@users.noreply.github.com>
2025-12-11 23:54:13 +0300

    feat: add sysadmin logo component

--- resources/views/filament/sysadmin/logo.blade.php
+++ resources/views/filament/sysadmin/logo.blade.php
@@ -1,3 +1,6 @@
 <div class="flex items-center gap-x-2">
-    <img src="{{ asset('images/logo.webp') }}" alt="Invest Expo CRM" class="h-8 w-auto" style="max-height: 2rem;" />
+    <img src="{{ asset('images/logo.webp') }}" alt="Invest Expo CRM" class="h-8 w-auto" />
+    <span class="text-lg font-semibold text-gray-950 dark:text-white hidden lg:block">
+        Invest Expo CRM
+    </span>
 </div>
